351810c048adef1e29faf528e51ce82ef0d3ad71
Steven Garcia <Steven.Garcia@pyramidci.com>
2022-08-31 15:07:52 -0400

    remove initial files.

--- simplepart-Test-Static-Markup/App_Start/BundleConfig.cs
+++ simplepart-Test-Static-Markup/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace simplepart_Test_Static_Markup
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                      "~/Content/EnglishPartsStyles.css")); 
+                      "~/Content/EnglishPartsStyles.min.css")); 
         }
     }
 }
